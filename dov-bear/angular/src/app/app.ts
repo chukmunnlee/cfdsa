1b25d45814be0b68cd965b7c9d00b5f35cadfa58
Lee Chuk Munn <chukmunnlee@gmail.com>
2025-10-27 15:43:47 +0800

    ADD: angular dov-bear

--- dov-bear/angular/src/app/app.ts
+++ dov-bear/angular/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+
+import { rnd } from './utils'
+
+const COUNT = 4
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('angular');
+  protected instanceName = 'dov-bear'
+
+  protected dovs: number[] = []
+
+  ngOnInit(): void {
+    //const total = parseInt(req.query['num']) || 4
+    this.dovs = rnd(14, COUNT)
+
+  }
 }
