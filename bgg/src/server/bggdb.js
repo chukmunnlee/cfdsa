4357836b92375a21710c29968cf848b378927ee0
Lee Chuk Munn <chukmunnlee@gmail.com>
2025-11-04 13:13:08 +0800

    ADD: bgg tng

---
+++ bgg/src/server/bggdb.js
@@ -0,0 +1,53 @@
+import mysql from 'mysql2/promise'
+
+const SQL_PING = "select 1"
+const SQL_SELECT_GAME_BY_NAME = 'select gid, name from game where name like ? order by name'
+const SQL_SELECT_GAME_BY_GID = 'select * from game where gid = ?'
+const SQL_SELECT_COMMENT_BY_GID = 'select user, c_text, rating from comment where gid = ? order by rating desc'
+
+export function BGGDatabase(username, password, host) {
+  this.username = username
+  this.password = password
+  this.host = host
+}
+
+BGGDatabase.prototype.connect = function() {
+  this.pool = mysql.createPool({
+    host: this.host,
+    port: 3306,
+    user: this.username,
+    password: this.password,
+    database: 'bgg',
+    connectionLimit: 3
+  })
+}
+
+BGGDatabase.prototype.findGamesByName = async function(name) {
+  const searchTerm = `%${name}%`
+  const result = await this.pool.query(SQL_SELECT_GAME_BY_NAME, [ searchTerm ])
+  return result[0]
+}
+
+BGGDatabase.prototype.findGameByGameId = async function(gid) {
+  const result = await this.pool.query(SQL_SELECT_GAME_BY_GID, [ gid ])
+  if (!!result[0].length)
+    return result[0][0]
+  return null
+}
+
+BGGDatabase.prototype.findCommentsByGameId = async function(gid) {
+  const result = await this.pool.query(SQL_SELECT_COMMENT_BY_GID, [ gid ])
+  return result[0]
+}
+
+
+BGGDatabase.prototype.ping = async function() {
+  await this.pool.query(SQL_PING)
+  return true
+}
+
+BGGDatabase.prototype.disconnect = function() {
+  return this.pool.end()
+}
+
+
---
+++ bgg/src/server/main.js
@@ -0,0 +1,93 @@
+import express from "express";
+
+import { BGGDatabase } from "./bggdb.js";
+
+
+const dbUser = process.env['BGG_DB_USER'] || 'root'
+const dbPassword = process.env['BGG_DB_PASSWORD'] || 'changeit'
+const dbHost = process.env['BGG_DB_HOST'] || '127.0.0.1'
+const appPort = parseInt(process.env['BGG_PORT']) || 3000
+
+const bggdb = new BGGDatabase(dbUser, dbPassword, dbHost)
+
+const app = express()
+
+app.get('/api/search', (req, resp) => {
+  if (!req.query['q'])
+    return resp.status(400)
+        .json({
+          timestamp: Date.now(),
+          message: 'Missing search term q'
+        })
+
+    bggdb.findGamesByName(req.query.q)
+      .then(result => resp.status(200).json(result))
+      .catch(error => 
+        resp.status(500)
+          .json({ 
+            timestamp: Date.now(),
+            message: 'Failed',
+            error
+          })
+      )
+})
+
+app.get('/api/game/:gid', (req, resp) => {
+  const gid = parseInt(req.params.gid)
+
+  Promise.all([ bggdb.findGameByGameId(gid), bggdb.findCommentsByGameId(gid) ])
+    .then(results => {
+      if (!results[0])
+        return resp.status(404)
+          .json({
+            timestamp: Date.now(),
+            message: `Gid not found: ${gid}`,
+          })
+      resp.status(200).json({
+        ...results[0],
+        comments: results[1]
+      }) 
+    })
+    .catch(error =>
+      resp.status(500)
+        .json({ 
+          timestamp: Date.now(),
+          message: 'Failed',
+          error
+        })
+    )
+})
+
+app.get('/healthz', (_, resp) => {
+  bggdb.ping()
+    .then(() => {
+      resp.status(200)
+        .json({ 
+          timestamp: Date.now(),
+          message: `Application is healthy`
+        })
+    })
+    .catch(error => {
+      resp.status(500)
+        .json({ 
+          timestamp: Date.now(),
+          message: `Application not healthy`,
+          error
+        })
+    })
+
+})
+
+app.use((req, resp) => {
+  resp.status(404)
+    .json({ 
+      timestamp: Date.now(),
+      message: `Resource '${req.url}' not found` 
+    })
+})
+
+bggdb.connect()
+app.listen(appPort, () => {
+  console.info(`Application started on port ${appPort} at ${new Date()}`)
+})
+
